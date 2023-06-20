b00b7a0178b55362082b155400101ddc8a4218f9
Arya723227926 <Arya723227926@163.com>
2023-06-20 11:38:17 +0800

    hzn,分站管理，根据”要求完成日期“查询任务单,格式修改

--- Substation-management-centre/src/main/java/com/example/substationmanagementcenter/sevice/impl/TaskServiceImpl.java
+++ Substation-management-centre/src/main/java/com/example/substationmanagementcenter/sevice/impl/TaskServiceImpl.java
@@ -1,21 +1,28 @@
 package com.example.substationmanagementcenter.sevice.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.substationmanagementcenter.entity.Task;
 import com.example.substationmanagementcenter.mapper.TaskMapper;
 import com.example.substationmanagementcenter.sevice.TaskService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
  * 退订 服务实现类
  * </p>
  *
- * @author gahgfa
+ * @author hzn
  * @since 2023-06-19
  */
 @Service
@@ -26,7 +33,17 @@ public class TaskServiceImpl extends ServiceImpl<TaskMapper, Task> implements Ta
 
 
     @Override
-    public List<Task> getTaskListByDeadline(Date date) {
-        return taskMapper.getTaskListByDeadline(date);
+    public PageInfo getTaskListByDeadline(Map<String,Object> map) throws ParseException {
+        PageHelper.startPage(Integer.valueOf((String)map.get("pageNum")),
+                Integer.valueOf((String)map.get("pageSize")));
+        QueryWrapper<Task> queryWrapper = new QueryWrapper<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date deadline =sdf.parse((String) map.get("deadline"));
+        queryWrapper.eq("deadline",deadline);
+
+        List<Task> res= taskMapper.selectList(queryWrapper);
+        PageInfo pageInfo = new PageInfo(res);
+        System.out.println(res);
+        return pageInfo;
     }
 }
