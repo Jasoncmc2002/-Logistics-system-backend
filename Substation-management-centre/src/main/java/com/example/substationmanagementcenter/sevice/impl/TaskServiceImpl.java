7873ec871e3aedbcec6ff1017d6be50dfa96ea52
Arya723227926 <Arya723227926@163.com>
2023-06-20 11:52:22 +0800

    hzn,分站管理，根据”要求完成日期“查询任务单,格式修改

--- Substation-management-centre/src/main/java/com/example/substationmanagementcenter/sevice/impl/TaskServiceImpl.java
+++ Substation-management-centre/src/main/java/com/example/substationmanagementcenter/sevice/impl/TaskServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -37,9 +38,20 @@ public class TaskServiceImpl extends ServiceImpl<TaskMapper, Task> implements Ta
         PageHelper.startPage(Integer.valueOf((String)map.get("pageNum")),
                 Integer.valueOf((String)map.get("pageSize")));
         QueryWrapper<Task> queryWrapper = new QueryWrapper<>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date deadline =sdf.parse((String) map.get("deadline"));
-        queryWrapper.eq("deadline",deadline);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(deadline);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        Date endTime = calendar.getTime();
+
+
+        queryWrapper.between("deadline",deadline,endTime);
+//        queryWrapper.eq("deadline",deadline);
 
         List<Task> res= taskMapper.selectList(queryWrapper);
         PageInfo pageInfo = new PageInfo(res);
