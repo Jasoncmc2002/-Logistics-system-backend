94bcacf28a6c5bdc1c6435590123124630fd5401
jarbom <379272203@qq.com>
2023-07-14 12:08:10 +0800

    更变 unsub

--- Customer-Service-Centre/src/main/java/com/example/customerservicecentre/feign/FeignApi.java
+++ Customer-Service-Centre/src/main/java/com/example/customerservicecentre/feign/FeignApi.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author YANG FUCHAO
@@ -19,21 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(name = "api-gateway")
 public interface FeignApi {
 
-  @RequestMapping(value = "/distribute/good/create")
+  @RequestMapping(value = "/distribute/good/create",method = RequestMethod.POST)
   HttpResponseEntity addGoods(Good good);
 
-  @RequestMapping(value = "/distribute/good/update")
+  @RequestMapping(value = "/distribute/good/update",method = RequestMethod.POST)
   HttpResponseEntity updateGoodByid(Good good);
 
-  @RequestMapping(value = "/distribute/good/delete/{id}")
+  @RequestMapping(value = "/distribute/good/delete/{id}",method = RequestMethod.POST)
   HttpResponseEntity deleteGoodByid(@PathVariable("id") String id);
 
-  @RequestMapping(value = "/distribute/good/{id}")
+  @RequestMapping(value = "/distribute/good/{id}",method = RequestMethod.POST)
   HttpResponseEntity getGoodByid(@PathVariable("id")Long id);
 
-  @RequestMapping(value = "/distribute/good/deleteBuyByGoodId")
+  @RequestMapping(value = "/distribute/good/deleteBuyByGoodId",method = RequestMethod.POST)
   HttpResponseEntity deleteBuyByGoodid(Map<String,Object > map);
 
-  @RequestMapping(value = "/distribute/good/updateBuyByGoodId")
+  @RequestMapping(value = "/distribute/good/updateBuyByGoodId",method = RequestMethod.POST)
   HttpResponseEntity updateBuyByid(Map<String,Object > map);
 }
